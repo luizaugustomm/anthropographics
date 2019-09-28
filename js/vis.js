5c1e0e1c20916be282f80a49eef3d6335a160f15
Luiz Augusto Morais <luizaugustomm@gmail.com>
2019-09-28 20:44:20 -0300

    Basic HTML template and 25 visualizations

---
+++ js/vis.js
@@ -0,0 +1,4 @@
+
+d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT1uwvE9Uk4vk1EWk90hSd398T5-taLeoM3qjlNwQfE-xi_5n6GREvV3Lh9Jr03xJw12Z8taMiKGfiU/pub?gid=0&single=true&output=csv').then(function(data) {
+  
+});
