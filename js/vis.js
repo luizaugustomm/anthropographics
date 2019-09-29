c5f5ad38b1abdba4239a0196add8aa840b1a2222
Luiz Augusto Morais <luizaugustomm@gmail.com>
2019-09-28 22:01:53 -0300

    Implement cards

--- js/vis.js
+++ js/vis.js
@@ -1,4 +1,36 @@
 
-d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT1uwvE9Uk4vk1EWk90hSd398T5-taLeoM3qjlNwQfE-xi_5n6GREvV3Lh9Jr03xJw12Z8taMiKGfiU/pub?gid=0&single=true&output=csv').then(function(data) {
-  
+d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQOVGTdhVf8xPIDHrpFCwxcQA4J4RzElbX3wW5IIw36Tap_339eWIk8TWB2Tka9zGOpkJdv_TYVYXlO/pub?gid=0&single=true&output=csv').then(function(data) {
+
+  const cards = d3.select('#cards')
+  .selectAll('div')
+  .data(data)
+  .enter()
+  .append('div')
+  .attr('class', 'col-sm-6 col-md-4 col-lg-3 mb-2 mx-4')
+  .attr('onclick', d => 'window.open("' + d.url + '")' )
+  .append('div')
+  .attr('class', d => 'card');
+
+cards
+  .append('img')
+  .attr('class', 'card-img-top')
+  .attr('src', d => d.img)
+  .attr('alt', d => d.title)
+  .style('height', '5em')
+  .style('object-fit', 'cover')
+
+const cardBodies = cards
+  .append('div')
+  .attr('class', 'card-body')
+
+cardBodies
+  .append('h5')
+  .attr('class', 'card-title')
+  .text(d => d.title)
+
+cardBodies
+  .append('div')
+  .attr('class', 'card-text')
+  .text(d => d.authors)
+
 });
