ce833631408f29b52e71d7726e7e97081a03a64a
Luiz Augusto Morais <luizaugustomm@gmail.com>
2019-11-22 22:24:48 -0300

    Stable version

--- js/vis.js
+++ js/vis.js
@@ -1,34 +1,51 @@
 
-d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQOVGTdhVf8xPIDHrpFCwxcQA4J4RzElbX3wW5IIw36Tap_339eWIk8TWB2Tka9zGOpkJdv_TYVYXlO/pub?gid=0&single=true&output=csv').then(function(data) {
+const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQOVGTdhVf8xPIDHrpFCwxcQA4J4RzElbX3wW5IIw36Tap_339eWIk8TWB2Tka9zGOpkJdv_TYVYXlO/pub?gid=0&single=true&output=csv'
 
+d3.csv(url).then(function(data) {
   const cards = d3.select('#cards')
-  .selectAll('div')
-  .data(data)
-  .enter()
-  .append('div')
-  .attr('class', 'col-sm-6 col-md-4 col-lg-3 mb-2')
-  .attr('onclick', d => 'window.open("' + d.id + '")' )
-  .append('div')
-  .attr('class', d => 'card');
+    .selectAll('div')
+    .data(data)
+    .enter()
+    .append('div')
+    .attr('class', 'col-sm-6 col-md-4 col-lg-3 mb-2 cc')
+    .append('div')
+    .attr('class', 'card')
+    .on('click', d => {
+      console.log("card " + d.title);
+      d3.select('#visualization-title')
+        .text(d.title)
 
-cards
-  .append('img')
-  .attr('class', 'card-img-top')
-  .attr('src', d => d.thumbnail)
-  .attr('alt', d => d.title)
+      d3.select('#visualization-image')
+        .attr('src', d.img)
+        .attr('alt', d.title)
+        .attr('class', 'img-fluid')
 
-const cardBodies = cards
-  .append('div')
-  .attr('class', 'card-body')
+      d3.select('#visualization-website')
+        .attr('href', d.url_source)
+        .text(d.authors)
 
-cardBodies
-  .append('h5')
-  .attr('class', 'card-title')
-  .text(d => d.title)
+      $('#visualization-modal').modal('show');
 
-cardBodies
-  .append('div')
-  .attr('class', 'card-text text-muted')
-  .text(d => d.authors)
+    })
+
+  cards
+    .append('img')
+    .attr('class', 'card-img-top')
+    .attr('src', d => d.thumbnail)
+    .attr('alt', d => d.title)
+
+  const cardBodies = cards
+    .append('div')
+    .attr('class', 'card-body')
+
+  cardBodies
+    .append('h5')
+    .attr('class', 'card-title')
+    .text(d => d.title)
+
+  cardBodies
+    .append('div')
+    .attr('class', 'card-text text-muted')
+    .text(d => d.authors)
 
 });
