82818f980d9e832a992c38f2b76824abe2ddbe85
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-02-13 17:36:22 -0300

    Implement filter feature

--- js/vis.js
+++ js/vis.js
@@ -1,62 +1,88 @@
 
 const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQOVGTdhVf8xPIDHrpFCwxcQA4J4RzElbX3wW5IIw36Tap_339eWIk8TWB2Tka9zGOpkJdv_TYVYXlO/pub?gid=0&single=true&output=csv'
 
-d3.csv(url).then(function(data) {
-  const cards = d3.select('#cards')
-    .selectAll('div')
-    .data(data)
-    .enter()
-    .append('div')
-    .attr('class', 'col-sm-6 col-md-4 col-lg-3 mb-2 cc')
-    .append('div')
-    .attr('class', 'card')
-    .on('click', d => {
-      d3.select('#visualization-title')
-        .text(d.title)
+function createCard(d) {
+  const img = `<img class="card-img-top" src="img/thumbnails/${d.id}-tn.png" alt="${d.title}">`
+  const cardTitle = `<h5 class="card-title">${d.title}</h5>`
+  const cardAuthors = `<div class="card-text text-muted">${d.authors}</div>`
+  const body = `<div class="card-body">${cardTitle}${cardAuthors}</div>`
+  const card = `<div class="card">${img}${body}</div>`
+  return card
+}
 
-      d3.select('#visualization-image')
-        .attr('src', d.img)
-        .attr('alt', d.title)
-        .attr('class', 'img-fluid')
+function updateCards(newData) {
+  const cardWrappers = d3.select('#cards')
+    .selectAll('.card-wrapper')
+    .data(newData)
+    .join('div')
+    .attr('class', 'col-sm-6 col-md-4 col-lg-3 mb-2 card-wrapper')
+        .html(createCard)
+        .on('click', updateCardModal)
+}
 
-      d3.select('#visualization-website')
-        .attr('href', d.url_source)
-        .text(d.authors)
+function updateCardModal(d) {
+  d3.select('#visualization-title')
+    .text(d.title)
 
-      d3.select('#visualization-dimensions')
-        .html(() => {
-          let tags = ''
-          tags += `<span class="badge badge-pill badge-secondary">granularity: ${d.granularity.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">specificity: ${d.specificity.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">coverage: ${d.coverage.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">authenticity: ${d.authenticity.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">realism: ${d.realism.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">situatedness: ${d.situatedness.toLowerCase()}</span>\n`
-          tags += `<span class="badge badge-pill badge-secondary">physicality: ${d.physicality.toLowerCase()}</span>\n`
-          return tags
-        })
+  d3.select('#visualization-image')
+    .attr('src', `img/raw/${d.id}.png`)
+    .attr('alt', d.title)
+    .attr('class', 'img-fluid')
 
-      $('#visualization-modal').modal('show');
+  d3.select('#visualization-website')
+    .attr('href', d.url_source)
+    .text(d.authors)
+
+  d3.select('#visualization-dimensions')
+    .html(() => {
+      let tags = ''
+      tags += `<span class="badge badge-pill badge-secondary">granularity: ${d.granularity.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">specificity: ${d.specificity.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">coverage: ${d.coverage.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">authenticity: ${d.authenticity.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">realism: ${d.realism.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">situatedness: ${d.situatedness.toLowerCase()}</span>\n`
+      tags += `<span class="badge badge-pill badge-secondary">physicality: ${d.physicality.toLowerCase()}</span>\n`
+      return tags
     })
 
-  cards
-    .append('img')
-    .attr('class', 'card-img-top')
-    .attr('src', d => d.thumbnail)
-    .attr('alt', d => d.title)
+  $('#visualization-modal').modal('show')
+}
 
-  const cardBodies = cards
-    .append('div')
-    .attr('class', 'card-body')
+function resetFilters() {  
+  d3.selectAll('.filter')
+    .property('checked', true)
+    .dispatch('change')
+}
 
-  cardBodies
-    .append('h5')
-    .attr('class', 'card-title')
-    .text(d => d.title)
 
-  cardBodies
-    .append('div')
-    .attr('class', 'card-text text-muted')
-    .text(d => d.authors)
+d3.csv(url).then(function(data) {
+  var totalVis = data.length
+  d3.select('#summary')
+    .text(`${totalVis} out of ${totalVis} visualizations`)
 
-});
+  updateCards(data)
+
+  d3.selectAll('.filter:checked')
+    .on('change', function() {
+      const dimensions = ['granularity', 'specificity', 'coverage',
+                          'authenticity', 'realism', 'physicality', 'situatedness']
+      let newData = data
+
+      dimensions.forEach(dimension => {
+        let changed = []
+        d3.selectAll(`.filter-${dimension}:checked`)
+          .each(function() {
+            changed.push(this.value)
+          })
+        newData = newData.filter(function(d) {
+          return changed.includes(d[dimension])
+        })
+      })
+
+      updateCards(newData)
+
+      d3.select('#summary')
+        .text(`${newData.length} out of ${totalVis} visualizations`)
+    })
+})
